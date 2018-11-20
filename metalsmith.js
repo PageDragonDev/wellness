18b077a4d857116b5b772d3374acf4e49c760a67
Jerry Holmes <jholmes@pagedragon.com>
2018-11-20 17:35:28 -0500

    Home page

--- metalsmith.js
+++ metalsmith.js
@@ -1,69 +1,61 @@
-var Metalsmith  = require('metalsmith');
-var stylus = require('metalsmith-stylus');
-var markdown    = require('metalsmith-markdown');
-var collections = require('metalsmith-collections');
-var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
-var msif = require('metalsmith-if');
-var watch = require('metalsmith-watch');
-var serve = require('metalsmith-serve');
-var moment = require("moment");
-var excerpts = require('metalsmith-better-excerpts');
-var marked = require('marked');
-var categories = require('./plugins/metalsmith-categories.js');
+/* eslint-disable */
 
-Metalsmith(__dirname) 
+const Metalsmith = require('metalsmith');
+const stylus = require('metalsmith-stylus');
+const markdown = require('metalsmith-markdown');
+const collections = require('metalsmith-collections');
+const layouts = require('metalsmith-layouts');
+const permalinks = require('metalsmith-permalinks');
+const msif = require('metalsmith-if');
+const watch = require('metalsmith-watch');
+const serve = require('metalsmith-serve');
+const moment = require('moment');
+const excerpts = require('metalsmith-better-excerpts');
+const marked = require('marked');
+const data = require('metalsmith-data');
+const categories = require('./plugins/metalsmith-categories.js');
+console.log('ENV:',process.env.ENV);
 
-.source('./src')    // source directory
-.destination('./build') // destination directory
-.clean(true)    // clean destination before
-.metadata({
-    moment: moment,
-    marked: marked
-})
-.use(collections({
-    raves: {
-        date: 'date',
-        reverse: true,
-        pattern: "**/_raves/*.md"
-    },
-    news: {
-        // date: 'date',
-        // reverse: true,
-        pattern: "**/_news/*.md"
-    },
-    buy: {
-        // date: 'date',
-        // reverse: true,
-        pattern: "**/_buy/*.md"
-    }
-})) // use `collections.posts` in layouts
-.use(markdown())   // transpile all md into html
-.use(categories({path:"category/"}))
-.use(categories({path:"service/", list:"services", layout_type:"service"}))
-.use(permalinks({           // change URLs to permalink URLs
-    relative: false           // put css only in /css
-  }))
-.use(stylus())
-.use(layouts({ // wrap layouts around html
-    engine: 'ejs', // use the layout engine you like
-    partials: "partials",
-    partialExtension: ".ejs",
-    default: "layout.ejs",
-    pattern: "**/*.html"
-}))
-.use(msif(process.env.ENV !== 'prod',watch({
+Metalsmith(__dirname)
+    .source('./src') // source directory
+    .destination('./build') // destination directory
+    .clean(true) // clean destination before
+    .metadata({
+        moment,
+        marked,
+    })
+    .use(data({}))
+    .use(collections({})) // use `collections.posts` in layouts
+    .use(markdown()) // transpile all md into html
+    .use(categories({ path: 'category/' }))
+    .use(categories({ path: 'service/', list: 'services', layout_type: 'service' }))
+    .use(permalinks({ // change URLs to permalink URLs
+        relative: false, // put css only in /css
+    }))
+    .use(stylus())
+    .use(layouts({ // wrap layouts around html
+        directory: "layouts",
+        engine: 'ejs', // use the layout engine you like
+        partials: 'partials',
+        partialExtension: '.ejs',
+        default: 'layout.ejs',
+        pattern: '**/*.html',
+        suppressNoFilesError: true,
+    }))
+    .use(msif(process.env.ENV !== 'prod', watch({
         paths: {
-            "${source}/**/*": true,
-            "layouts/**/*": "**/*.md",
+            '${source}/**/*': true,
+            'layouts/**/*': '**/*.md',
         },
         livereload: process.env.ENV !== 'prod',
     })))
-    .use(msif(process.env.ENV !== 'prod',serve({
-        host: "0.0.0.0",
+    .use(msif(process.env.ENV !== 'prod', serve({
+        host: '0.0.0.0',
         port: 8080,
-        verbose: true
+        verbose: true,
     })))
-.build(function(err) {      // build process
-    if (err) throw err;       // error handling is required
-  });
+    .build((err) => { // build process
+        if (err) {
+            console.log(err);
+        } // error handling is required
+    });
--- src/index.md
+++ src/index.md
@@ -1,6 +1,48 @@
 ---
 title: Home page
 layout_type: home
-banner: 
+banner: home-header.jpg
+top_callout: '### Face, Breast, Body, Skin
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+specialty1_icon: nutrient-therapy.png
+specialty2_icon: hormone-balance.png
+specialty3_icon: chiropractic.png
+specialty4_icon: muscle-therapy.png
+specialty1_text: '### Nutrient Therapy
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi. Donec vitae luctus nisi. Mauris gravida sollicitudin diam,
+  eu tempus eros sagittis sed. Aenean metus arcu, maximus non nibh vitae, scelerisque blandit leo. Etiam ante mauris, euismod nec mauris eget, egestas faucibus orci.'
+specialty2_text: '### Hormone Balance
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi. Donec vitae luctus nisi. Mauris gravida sollicitudin diam,
+  eu tempus eros sagittis sed. Aenean metus arcu, maximus non nibh vitae, scelerisque blandit leo. Etiam ante mauris, euismod nec mauris eget, egestas faucibus orci.'
+specialty3_text: '### Chiropractic
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi. Donec vitae luctus nisi. Mauris gravida sollicitudin diam,
+  eu tempus eros sagittis sed. Aenean metus arcu, maximus non nibh vitae, scelerisque blandit leo. Etiam ante mauris, euismod nec mauris eget, egestas faucibus orci.'
+specialty4_text: '### Muscle Therapy
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi. Donec vitae luctus nisi. Mauris gravida sollicitudin diam,
+  eu tempus eros sagittis sed. Aenean metus arcu, maximus non nibh vitae, scelerisque blandit leo. Etiam ante mauris, euismod nec mauris eget, egestas faucibus orci.'
+schedule_form: Form goes here. Yada yada.
+green_text: '### Latest Videos
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi.'
+green_callout1: Video Here
+green_callout2: Video Here
+green_callout3: Video Here
+saying_icon: saying.png
+saying_text: '### Rose
+
+    #### patient
+
+    "I look and feel great! I wish I would have done this years ago! The team at Centered On Wellness is ultra-professional, talented, and made me feel like I was in my home."'
 ---
 
+# Welcome To Centered on wellness
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec porttitor mi. Donec vitae luctus nisi. Mauris gravida sollicitudin diam, eu tempus eros sagittis sed. Aenean metus arcu, maximus non nibh vitae, scelerisque blandit leo. Etiam ante mauris, euismod nec mauris eget, egestas faucibus orci.
